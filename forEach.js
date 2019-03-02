632a0889c6b999d6177d0dc5b4d9be699a28c114
April Copes <antdevelopment1@gmail.com>
2019-03-02 12:49:19 -0500

    callback for manual for each function

---
+++ forEach.js
@@ -0,0 +1,20 @@
+// Example of forEach using all parameters
+var strings = ['I', 'love', 'puppies'];
+var result = '';
+
+function forEach(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, arr);  
+    }
+   return result;
+};
+
+function doWork(word, index, array) {
+    if(array.length - 1 !== index) {
+        return result += word + " ";
+    } else {
+        return result += word + '!!!';
+    } 
+};
+
+console.log(forEach(strings, doWork));
