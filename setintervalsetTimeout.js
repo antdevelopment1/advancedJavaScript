de12aa7892b1fc7d95281f4e508f72c16812299d
April Copes <antdevelopment1@gmail.com>
2019-03-03 14:18:24 -0500

    Adds set timeout example

--- setintervalsetTimeout.js
+++ setintervalsetTimeout.js
@@ -8,7 +8,7 @@ function countDown(countFrom) {
     var interval = setInterval(function(){
         countFrom--;
         if (countFrom > 0) {
-            console.log(countFrom);
+            console.log('Timer:',countFrom);
         } else {
             console.log('Ring Ring Ring!!!');
             clearInterval(interval);
@@ -16,4 +16,20 @@ function countDown(countFrom) {
     }, 1000)
 }
 
-console.log(countDown(10))
+// console.log(countDown(10))
+
+
+// Set Timeout Example
+function first() {
+    setTimeout(function() {
+        console.log('I should come first but my timer is higher than the second function so I will get cut in line');
+    }, 1000)
+}
+console.log(first())
+
+function second() {
+    setTimeout(() => {
+        console.log("But Im actually going to come first even though I'm second because my delay will put me on the queue first. Haha");
+    }, 500);
+}
+console.log(second())
