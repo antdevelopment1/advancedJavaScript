02ab9c1376dbec785752cd61e22558a5b987783c
April Copes <antdevelopment1@gmail.com>
2019-03-03 19:19:57 -0500

    Adds abstract example of using call in the wild to loop through div's with certain text values

--- call.js
+++ call.js
@@ -16,7 +16,7 @@ var cat = {
 console.log(dog.sayHi())
 console.log(cat.sayHi())
 
-// 
+// Refactored using call explicit binding
 var dog = {
     name: "Sam",
     sayHi: function () {
@@ -36,4 +36,41 @@ var puppy = {
 console.log(dog.sayHi())
 console.log(dog.sayHi.call(cat))
 console.log(dog.sayHi.call(puppy))
+
+
+var universal = {
+    sayHi: function () {
+        console.log("Hi my name is " + this.name);
+    }
+}
+
+var dragon = {
+    name: "Dragon"
+}
+
+var witch = {
+    name: "Hermione Granger"
+}
+
+var wizard = {
+    name: "Albus Dumbledore"
+}
+
+
+console.log(universal.sayHi.call(dragon));
+console.log(universal.sayHi.call(witch));
+console.log(universal.sayHi.call(wizard));
+
+// Imagine we want to select all the divs on a page
+var divs = document.getElementsByTagName('div');
+// How can we find all the divs that have the text "Hello". Using filter would be nice.
+// divs.filter would return undefined because dis isn't not an array and filter is an array method
+// We can convert an array like object into an array by using the slice method and making a copy of the array
+
+// Let's use the slice method on arrays, but instead of the target of slice (the keyword this) being that array,
+// let's set the target of the keyword of 'this' to be our div's array like object
+var divArray = [].slice.call(divs);
+divArray.filter(function(val) {
+    return val.innerText === "Hello";
+})
 
