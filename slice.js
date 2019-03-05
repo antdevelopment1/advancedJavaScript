f83a4bd4ff1ffb040fad5e157d11cca8c83be265
April Copes <antdevelopment1@gmail.com>
2019-03-04 20:43:23 -0500

    adds a slice method into object

--- slice.js
+++ slice.js
@@ -1,20 +1,22 @@
 // Trying to recreate a slice method
-function sliceMethod(start, end) {
-
-    let arr = [1,2,3,4,5];
-    let newArr = [];
+let obj = {
+    slice: function (start, end) {
     
-    for (let i = 0; i < arr.length; i++) {
-        if (end === undefined) {
-            end = arr.length;
-        }
-        for (let j = start; j < end; j++) {
-            if (newArr.indexOf(arr[j]) === -1) {
-                newArr.push(arr[j]);
+        let arr = [1,2,3,4,5];
+        let newArr = [];
+        
+        for (let i = 0; i < arr.length; i++) {
+            if (end === undefined) {
+                end = arr.length;
+            }
+            for (let j = start; j < end; j++) {
+                if (newArr.indexOf(arr[j]) === -1) {
+                    newArr.push(arr[j]);
+                }
             }
         }
+        return newArr;
     }
-    return newArr;
 }
 
-console.log(sliceMethod(1,3))
+console.log(obj.slice(1))
